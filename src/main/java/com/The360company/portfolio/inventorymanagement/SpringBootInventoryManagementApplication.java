172a303b26a3640d06a5d98f210f5331ab7eba62
SYAHRUL360 <not.syahrul@outlook.co.id>
2021-06-25 15:39:54 +0700

    add SpringBootServletInitializer to main method

--- src/main/java/com/The360company/portfolio/inventorymanagement/SpringBootInventoryManagementApplication.java
+++ src/main/java/com/The360company/portfolio/inventorymanagement/SpringBootInventoryManagementApplication.java
@@ -2,9 +2,10 @@ package com.The360company.portfolio.inventorymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootInventoryManagementApplication {
+public class SpringBootInventoryManagementApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootInventoryManagementApplication.class, args);
